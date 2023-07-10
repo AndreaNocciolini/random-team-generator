6ab19b3ea575afa66306ef70c6afd8ef42438c61
Andrea Nocciolini <andreanocciolini93@gmail.com>
2023-07-10 22:14:35 +0200

    fix: add Bebas Neue Font

--- src/components/HomeViewComponents/TeamList.vue
+++ src/components/HomeViewComponents/TeamList.vue
@@ -4,21 +4,22 @@ import '../../assets/TeamList.scss'
 
 
 <template>
-  <div class="addPlayerModal">
-    <input class="addPlayerInput" v-model="player" placeholder="Add a new player" /> <br /><br />
-    <input class="addPlayerButton" type="submit" @click="storePlayer" value="Add Player" />
-    <button class="getPlayersButton" @click="getPlayers">Get Player List</button>
-  </div>
-  
-  <div v-if=this.players.length>
-    <ul class="list">
-      <li v-for="(player, i) in players" :key="i" class="d-flex justify-content-between w-100 m-1">
-        {{ player }} <input type="submit" @click="deletePlayer(i)" value="Remove" />
-      </li>
-    </ul>
-  </div>
-  <div v-else class="d-flex justify-content-center flex-column">
-    <h1>Warning: no players saved</h1>
+  <div class="d-flex flex-row justify-content-around w-100">
+    <div class="addPlayerModal">
+      <input class="addPlayerInput" v-model="player" placeholder="Add a new player" /> <br /><br />
+      <input class="addPlayerButton" type="submit" @click="storePlayer" value="Add Player" />
+      <button class="getPlayersButton" @click="getPlayers">Get Player List</button>
+    </div>
+
+    <div v-if=this.players.length>
+      <ul class="list">
+        <b-table striped hover :items="items"></b-table>
+          {{ player }} <input type="submit" @click="deletePlayer(i)" value="Remove" />
+      </ul>
+    </div>
+    <div v-else class="d-flex justify-content-center flex-column">
+      <h1>Warning: no players saved</h1>
+    </div>
   </div>
 </template>
 
@@ -39,7 +40,7 @@ export default {
       }
     },
 
-    deletePlayer(index){
+    deletePlayer(index) {
       this.players.splice(index, 1);
       localStorage.setItem("players", JSON.stringify(this.players));
     },
